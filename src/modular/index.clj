1479bed5902bf325756e8286e74632b1c4050862
Malcolm Sparks <malcolm@congreve.com>
2014-04-03 14:34:05 +0100

    Extracted Index from protocols

---
+++ src/modular/index.clj
@@ -0,0 +1,23 @@
+;; Copyright © 2014 JUXT LTD.
+
+(ns ^{:clojure.tools.namespace.repl/unload false
+      :clojure.tools.namespace.repl/load false}
+  modular.index)
+
+;; A component that satisfying this Index protocol declares that it
+;; provides an index for components satisfying one or more
+;; protocols. For example, a Menu component may be an index of all
+;; MenuItem components. A contents or index page could be generated from
+;; all the Page components present in the system.
+
+;; An application can use these declarations to infer a dependency tree
+;; between the components satisfying Index and components satisfying one
+;; or more of the protocols declared by it.
+
+;; One of the characteristics of modular applications is dynamic
+;; component discovery that allows applications to be extended via the
+;; introduction of new components. Therefore we must ensure that the
+;; coupling between existing components is minimised.
+
+(defprotocol Index
+  (satisfying-protocols [this]))
