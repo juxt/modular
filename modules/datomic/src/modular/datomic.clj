986d1330b299aefd9fe1eae8d91269a3789d4d36
Malcolm Sparks <malcolm@juxt.pro>
2014-04-03 11:14:07 +0100

    Component to add Datomic functions

--- modules/datomic/src/modular/datomic.clj
+++ modules/datomic/src/modular/datomic.clj
@@ -62,18 +62,38 @@
 (defrecord DatomicSchema [res]
   component/Lifecycle
   (start [this]
-    (let [conn (get-in this [:connection])]
-      (with-open [rdr (java.io.PushbackReader. (io/reader res))]
-        @(d/transact conn
-                     (binding [clojure.tools.reader/*data-readers*
-                               {'db/id datomic.db/id-literal
-                                'db/fn datomic.function/construct
-                                'base64 datomic.codec/base-64-literal}]
-                       (clojure.tools.reader/read (indexing-push-back-reader rdr))))))
+    (with-open [rdr (java.io.PushbackReader. (io/reader res))]
+      @(d/transact (:connection this)
+                   (binding [clojure.tools.reader/*data-readers*
+                             {'db/id datomic.db/id-literal
+                              'db/fn datomic.function/construct
+                              'base64 datomic.codec/base-64-literal}]
+                     (clojure.tools.reader/read (indexing-push-back-reader rdr)))))
     this)
   (stop [this] this))
 
 (defn new-datomic-schema [res]
   (component/using
    (->DatomicSchema res)
+   [:connection]))
+
+
+(defn create-functions [functions]
+  (vec
+   (for [[ident {:keys [doc params path]}] functions]
+     {:db/id (d/tempid :db.part/user)
+      :db/ident ident
+      :db/doc doc
+      :db/fn (d/function {:lang "clojure" :params params :code (slurp (io/resource path))})})))
+
+(defrecord DatomicFunctions [functions]
+  component/Lifecycle
+  (start [this]
+    @(d/transact (:connection this) (create-functions functions))
+    this)
+  (stop [this] this))
+
+(defn new-datomic-functions [functions]
+  (component/using
+   (->DatomicFunctions functions)
    [:connection]))
