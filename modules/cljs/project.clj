908f4f51e1ff7ed74df44a0c1a4be1f9ca06175c
Malcolm Sparks <malcolm@juxt.pro>
2014-04-07 19:28:17 +0100

    Added dependency fro cljs to shadow-build

--- modules/cljs/project.clj
+++ modules/cljs/project.clj
@@ -1,10 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/cljs "0.1.0"
+(defproject juxt.modular/cljs "0.1.1"
   :description "A modular extension that provides support for ClojureScript building and Javascript serving"
   :url "https://github.com/juxt/modular/tree/master/modules/cljs"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[prismatic/schema "0.2.1"]
                  [juxt/modular "0.3.0"]
-                 [juxt.modular/bidi "0.3.0"]])
+                 [juxt.modular/bidi "0.3.0"]
+                 [thheller/shadow-build "0.5.0" :exclusions [org.clojure/clojurescript]]])
