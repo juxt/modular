5cce381e0d60eb185c0286e13f2cedd854702f71
Malcolm Sparks <malcolm@juxt.pro>
2014-04-28 02:05:16 +0100

    Remove redundant file

--- modules/bidi/src/modular/bidi/menu.clj
+++
@@ -1,8 +0,0 @@
-;; Copyright © 2014 JUXT LTD.
-
-(ns modular.bidi.menu
-  (:require
-   [com.stuartsierra.component :as component]
-   [modular.template :refer (TemplateModel)]
-   [bidi.bidi :refer (path-for)]
-   [hiccup.core :refer (html)]))
