3cc8f53150d8935272bf1efe5b0551ed037b27bd
Malcolm Sparks <malcolm@juxt.pro>
2014-04-29 15:47:29 +0100

    Support for multiple templates
    Non-default templates can be indicated with a keyword in wrap-template.

--- modules/template/src/modular/template.clj
+++ modules/template/src/modular/template.clj
@@ -13,30 +13,40 @@
 (defprotocol Templater
   (render-template [_ template model]))
 
-(defrecord SingleTemplate [template]
+(defrecord Template [template]
   RingBinding
   (ring-binding [this req]
-    {::template
-     (fn [req content]
-       (let [model (apply merge
-                          (for [[k v] this
-                                :when (satisfies? TemplateModel v)]
-                            {k (template-model v {:request req})}))]
-         (debugf "Template model, prior to merge with content, is %s" (pr-str model))
-         (debugf "After will be: %s" (merge model content))
-         (render-template
-          (:templater this)
-          template
-          (merge-with merge model content))))}))
+    {::template {(:key this)
+                 (fn [req content]
+                   (let [model (apply merge
+                                      (for [[k v] this
+                                            :when (satisfies? TemplateModel v)]
+                                        {k (template-model v {:request req})}))]
+                     (debugf "Template model, prior to merge with content, is %s" (pr-str model))
+                     (debugf "After will be: %s" (merge model content))
+                     (render-template
+                      (:templater this)
+                      template
+                      (merge-with merge model content))))}}))
 
-(defn new-single-template
+(defn new-template
   "The template argument can be a path, resource, file or whatever is
   supported by the dependant templater."
   [& {:as opts}]
   (component/using
    (->> opts
         (s/validate {:template s/Any})
-        map->SingleTemplate)
+        map->Template)
+   [:templater]))
+
+(defn new-keyed-template
+  "The template argument can be a path, resource, file or whatever is
+  supported by the dependant templater."
+  [& {:as opts}]
+  (component/using
+   (->> opts
+        (s/validate {:key s/Keyword :template s/Any})
+        map->Template)
    [:templater]))
 
 ;; TODO: The issue with SingleTemplate is that it is static, it is given
@@ -50,13 +60,17 @@
   use it to process the response of the delegate handler. The initial
   response is considered to be a template model, and merged with other
   template models from other components."
-  [h]
-  (fn [req]
-    (if-let [merge-with-template (::template req)]
-      (let [resp (h req)]
-        (merge resp {:body (merge-with-template req resp)}))
-      (throw (ex-info "wrap-template expects that a template is bound in the request with key :modular.template/template" {})))))
-
+  ([h & [k]]
+     (fn [req]
+       (if-let [merge-with-template (get-in req [::template k])]
+         (let [resp (h req)]
+           (merge resp {:body (merge-with-template req resp)}))
+         (throw
+          (if (::template req)
+            (if k
+              (ex-info (format "No template with key %s" k) {:key k})
+              (ex-info (format "No default template") {}))
+            (ex-info "wrap-template expects that a template is included in the system" {:key k})))))))
 
 (defrecord TemplateModelMap []
   TemplateModel
