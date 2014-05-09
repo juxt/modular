dcb79d859fba06c93309529d6ef518aed54897be
Malcolm Sparks <malcolm@juxt.pro>
2014-05-09 14:23:15 +0100

    Maker improvements

--- modules/maker/src/modular/maker.clj
+++ modules/maker/src/modular/maker.clj
@@ -2,19 +2,38 @@
 
 (ns modular.maker)
 
-(defn- make-args
+(defn make-args
   "In modular, constructors use the variadic keyword arguments
-  call-convention. This function allows us to formulate these arguments
-  from a config map and a list of specified keys. Each key can take a
-  default value, or nil if no value should be passed. The value will
-  then be determined by the constructor itself, not the calling code."
-  [cfg & {:as args}]
-  (as-> args %
-        (merge % cfg)
-        (select-keys % (keys args))
-        (seq %)
-        (remove (comp nil? second) %)
-        (apply concat %)))
+call-convention. This function allows us to formulate these arguments
+from a config map and a list of specified keys. Each key can take a
+default value, which can be nil, in which case no value is passed and
+the constructor will determine the value, unless the key exists in the
+config. If the value is the special value of :modular.maker/required, an
+exception will be thrown if no value can be found in the config map.
+
+ In cases where there is no a correspondence between the keyword
+argument required by the constructor and the config map, a mapping
+can be given. Instead of using a keyword for the key, a map with a
+single kv entry can be given. The key is the one expected by the
+constructor. The value can be key or vector of keys, representing the
+path in the config where the value should be found.
+
+See modular.maker-tests/make-args-test for examples."
+  [cfg & args]
+  (apply concat
+         (for [[k dv]
+               (partition 2 args)]
+           (let [v (get-in cfg
+                           (cond (keyword? k) [k]
+                                 (associative? k)
+                                 (let [path (second (first (seq k)))]
+                                   (cond (keyword? path) [path]
+                                         (vector? path) path))))]
+             (cond
+              (and (= dv :modular.maker/required) (nil? v))
+              (throw (ex-info "Required" {}))
+              (keyword? k) [k (or v dv)]
+              (associative? k) [(ffirst (seq k)) (or v dv)])))))
 
 (defn make
   "Call the constructor with default keyword arguments, each of which is
---
+++ modules/maker/test/modular/maker_tests.clj
@@ -0,0 +1,23 @@
+;; Copyright © 2014 JUXT LTD.
+
+(ns modular.maker-tests
+  (:use clojure.test modular.maker))
+
+(deftest make-args-test
+  (testing "Default if no override"
+    (is (= '(:a 1) (make-args {} :a 1))))
+  (testing "No specified value, no arg"
+    (is (= '() (make-args {:a 2}))))
+  (testing "Config overrides specified value"
+    (is (= '(:a 2) (make-args {:a 2} :a 1))))
+  (testing "Nil selects config"
+    (is (= '(:a 2) (make-args {:a 2} :a nil))))
+  (testing "Nil selects config"
+    (is (= '(:a 2) (make-args {:a 2} :a :required))))
+  (testing "Exception thrown on required"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (make-args {} :a :modular.maker/required))))
+  (testing "Mapping with a keyword"
+    (is (= '(:a 2) (make-args {:b 2} {:a :b} 1))))
+  (testing "Mapping with a vector path"
+    (is (= '(:a 2) (make-args {:b {:c 2}} {:a [:b :c]} 1)))))
