0c6c649fc5cbd441cea7f593d79010d9b59ac53d
Malcolm Sparks <malcolm@juxt.pro>
2014-05-09 16:43:54 +0100

    Menu security integration

--- modules/menu/project.clj
+++ modules/menu/project.clj
@@ -8,4 +8,5 @@
   :dependencies [[com.stuartsierra/component "0.2.1"]
                  [juxt.modular/bidi "0.4.0-SNAPSHOT"]
                  [juxt.modular/template "0.1.0-SNAPSHOT"]
-                 [prismatic/schema "0.2.1"]])
+                 [prismatic/schema "0.2.1"]
+                 [cylon "0.2.0-SNAPSHOT"]])
--- modules/menu/src/modular/menu.clj
+++ modules/menu/src/modular/menu.clj
@@ -5,19 +5,39 @@
    [com.stuartsierra.component :as component]
    [modular.template :refer (TemplateModel)]
    [bidi.bidi :refer (path-for)]
-   [hiccup.core :refer (html)]))
+   [hiccup.core :refer (html)]
+   [cylon.core :refer (allowed-handler?)]
+   [schema.core :as s]))
+
+(defn show-menu-item?
+  "This is an example of how useful it is to wrap handlers in records
+  that implement the AuthorizedHandler protocol. In this case, we can
+  call authorized-handler? on any given handler to see whether it would
+  be authorized with this request. We use this result to avoid
+  displaying menu-items that are not available. We could alternatively
+  use this information to disable or grey-out the menu-item."
+  [req {:keys [handler label]}]
+  (allowed-handler? handler req))
 
 (defprotocol MenuItems
   (menu-items [_ context]))
 
+;; If there is a protection system... see accounting2 eae2f02 src/juxt/accounting/menu.clj
+#_(when-let [{:keys [handlers]} (-> this :protection-system :login-form)]
+                  [(if-not (:cylon.core/session req)
+                     {:label "Login" :order "Z" :handler (:login handlers)}
+                     {:label "Logout" :order "Z" :handler (:logout handlers)})])
+
 (defrecord MenuIndex []
   MenuItems
   (menu-items [this context]
+    (s/validate {:request {s/Keyword s/Any}} context)
     (->> this
          vals
          (filter (partial satisfies? MenuItems))
          (mapcat #(menu-items % context))
          (remove nil?)
+;;         (filter (partial show-menu-item? (:request context)))
          (sort-by :order)
          (group-by :parent)
          seq
@@ -30,7 +50,6 @@
   TemplateModel
   (template-model [this {{routes :modular.bidi/routes :as req} :request :as context}]
     (let [menu (menu-items (:menu-index this) context)]
-      (println "menu is " menu)
       {:menu
        (html
         (apply concat
