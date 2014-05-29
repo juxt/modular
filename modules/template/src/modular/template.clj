3b472ba006ae767d2de51088f87ec6ab98df38df
Malcolm Sparks <malcolm@juxt.pro>
2014-05-29 11:41:19 +0100

    Remove unnecessary comment

--- modules/template/src/modular/template.clj
+++ modules/template/src/modular/template.clj
@@ -13,12 +13,6 @@
 (defprotocol Templater
   (render-template [_ template model]))
 
-;; An issue with Template is that it is static, it is given
-;; the template as an argument. It is envisaged that other records,
-;; other than SingleTemplate, which will switch the template based on
-;; the request, can be developed. For now, if you have multiple
-;; templates, create a SingleTemplate record for each one.
-
 (defrecord Template [template]
   RingBinding
   (ring-binding [this req]
