41146511ffd6a4332a008eb103907ed2d80628f8
Malcolm Sparks <malcolm@juxt.pro>
2014-06-05 01:36:35 +0100

    Add comments

--- lein-template/src/leiningen/new/modular/website.clj
+++ lein-template/src/leiningen/new/modular/website.clj
@@ -8,21 +8,44 @@
 
 {{! "We change the delimiters to avoid conflicting with nested destructuring in the code below" }}
 {{=<% %>=}}
-(defn index []
+
+(defn index
+  "Define a Liberator resource map for the index (home) page of the website"
+  []
   {:available-media-types #{"text/html"}
-   :handle-ok (fn [{{routes :modular.bidi/routes} :request}]
-                (html
-                 [:head
-                  [:title "Welcome"]]
-                 [:body
-                  [:h1 "Hello World! from <% name %>"]
-                  [:h2 "Links"]
-                  [:p [:a {:href (path-for routes ::main)} "Home"]]
-                  ]))})
+   :handle-ok
+   (fn [{{routes :modular.bidi/routes} ; it is common to destructure the
+                                       ; bidi route structure from the
+                                       ; request
+         :request}]
+     (html
+      [:head
+       [:title "Welcome"]]
+      [:body
+       [:h1 "Hello World! from <% name %>"]
+       [:h2 "Links"]
+       [:p [:a {:href (path-for routes ::main)} "Home"]]
+       ]))})
+
+;; Consider the component below. It is defined by defrecord.
+;; It satisfies 2 protocols: modular.bidi.WebService and modular.ring.RingHandler
+
+;; To satisfy modular.bidi.WebSerivce a component must provide the following:
+;;   ring-handler-map: A map between keywords and Ring handlers
+;;   routes: A bidi route structure, where terminals are keywords
+;;              - these keywords correspond to the keys in ring-handler-map
+;;   uri-context: Usually an empty string, but acts as a prefix to the route structure
+
+;; By also satisfying the RingHandler protocol, this component can be
+;; made a direct dependency of a Ring-compatible web server like
+;; http-kit or Jetty. Whether this is the only web-serving component in
+;; an application, or one of many (composed together by a router),
+;; depends on the system's dependency map defined in system.clj
+
+;; Finally there is the constructor, a function which creates an
+;; instance of the component record.
 
 (defrecord Website []
-  ;; Components such as this provide routes and handlers under an
-  ;; optional uri prefix (called the uri-context)
   WebService
   (ring-handler-map [_]
     {::main (-> (index) resource)})
@@ -31,11 +54,6 @@
 
   (uri-context [_] "")
 
-  ;; By satisfying the RingHandler protocol, this component can be made
-  ;; a direct dependency of a Ring-compatible web server like http-kit
-  ;; or Jetty. Whether this is the only web-serving component in an
-  ;; application, or one of many (composed together by a router),
-  ;; depends on the system's dependency map defined in system.clj
   RingHandler
   (ring-handler [this] (as-ring-handler this)))
 
