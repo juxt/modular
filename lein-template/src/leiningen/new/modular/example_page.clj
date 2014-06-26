7976fa95d749a89879f810a8e4f8b3ce7cb8a685
Malcolm Sparks <malcolm@juxt.pro>
2014-06-26 06:36:20 +0100

    Two example pages

--- lein-template/src/leiningen/new/modular/example_page.clj
+++ lein-template/src/leiningen/new/modular/example_page.clj
@@ -1,18 +1,26 @@
 (ns {{name}}.example-page
   (:require
-    [modular.menu :refer (MenuItems)]
-    [modular.bidi :refer (WebService)])
-)
+   [clojure.string :as str]
+   [modular.menu :refer (MenuItems)]
+   [modular.bidi :refer (WebService)]))
 
-(defrecord ExamplePage []
+(defrecord ExamplePage [label content key path]
   MenuItems
-  (menu-items [this] [{:label "Page" :target ::page}])
+  (menu-items [this] [{:label label :target key}])
   WebService
-  (request-handlers [_] {::page (fn [req] {:status 200 :body "PAGE"})})
-  (routes [_] ["/" {"page" ::page}])
-  (uri-context [_] "")
-)
+  (request-handlers [_]
+    {key (fn [req] {:status 200
+                    :headers {"Content-Type" "text/html"}
+                    :body (str "<h2>" content "</h2>")})})
+  (routes [_] ["" {path key}])
+  (uri-context [_] ""))
 
-(defn new-example-page []
-  (->ExamplePage)
-  )
+(defn new-example-page [& {:as opts}]
+  (let [label (or (:label opts) "Page")
+        sanitized (str/replace label #"\s+" "")]
+    (->> opts
+         (merge {:label label
+                 :text "Blank"
+                 :key (keyword (.toLowerCase sanitized))
+                 :path (str "/" sanitized)})
+         map->ExamplePage)))
