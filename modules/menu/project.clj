3fc750b92eefe4a588b20c82291a17bb3ecd100c
Malcolm Sparks <malcolm@juxt.pro>
2014-08-05 12:06:56 +0100

    Ready to release menu 0.5.2

--- modules/menu/project.clj
+++ modules/menu/project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/menu "0.5.2-SNAPSHOT"
+(defproject juxt.modular/menu "0.5.2"
   :description "A modular extension that provides support for menus, navbars, and other navigation elements that house hyperlinks."
   :url "https://github.com/juxt/modular/tree/master/modules/nav"
   :license {:name "The MIT License"
@@ -8,4 +8,4 @@
   :dependencies [[com.stuartsierra/component "0.2.1"]
                  [prismatic/schema "0.2.1"]
                  ;; menu extends Cylon's login-form
-                 [cylon "0.4.0-SNAPSHOT"]])
+                 [cylon "0.4.0"]])
