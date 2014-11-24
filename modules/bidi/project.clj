b08da30bc5194c810b225e41648bd108ae826122
Malcolm Sparks <malcolm@juxt.pro>
2014-11-24 11:51:48 +0000

    Updated to latest version of bidi

--- modules/bidi/project.clj
+++ modules/bidi/project.clj
@@ -6,7 +6,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/component "0.2.1"]
-                 [bidi "1.10.3"]
+                 [bidi "1.11.0"]
                  [juxt.modular/ring "0.5.1"]
                  [prismatic/schema "0.2.1"]
                  [prismatic/plumbing "0.2.2"]]
