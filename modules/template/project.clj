23a3c2a8141aac9e11d612c9d96019bf8c729572
Malcolm Sparks <malcolm@juxt.pro>
2014-11-25 09:48:36 +0000

    Template release 0.6.0

--- modules/template/project.clj
+++ modules/template/project.clj
@@ -1,9 +1,8 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/template "0.6.0-SNAPSHOT"
+(defproject juxt.modular/template "0.6.0"
   :description "A modular extension that provides support for templates (Mustache, etc.)"
   :url "https://github.com/juxt/modular/tree/master/modules/template"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[com.stuartsierra/component "0.2.2"]
-                 ])
+  :dependencies [[com.stuartsierra/component "0.2.2"]])
