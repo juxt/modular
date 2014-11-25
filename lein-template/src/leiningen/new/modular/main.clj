61a9c12c02351e41a67b48e7284968a7528fb9dc
Malcolm Sparks <malcolm@juxt.pro>
2014-11-25 09:38:30 +0000

    Use co-dependency in main.clj

--- lein-template/src/leiningen/new/modular/main.clj
+++ lein-template/src/leiningen/new/modular/main.clj
@@ -7,19 +7,18 @@
   ;; We eval so that we don't AOT anything beyond this class
   (eval '(do (require '{{name}}.system)
              (require '{{name}}.main)
-             (require 'com.stuartsierra.component)
+             (require 'tangrammer.component.co-dependency)
              (require 'clojure.java.browse)
 
              (println "Starting {{name}}")
 
-             (let [system (com.stuartsierra.component/start
+             (let [system (tangrammer.component.co-dependency/start-system
                            ({{name}}.system/new-production-system))]
 
                (println "System started")
                (println "Ready...")
 
-               {{!
-                 (let [url (format "http://localhost:%d/" (-> system :webserver :port))]
-                   (println (format "Browsing at %s" url))
-                   (clojure.java.browse/browse-url url)
-                   )}}))))
+               (let [url (format "http://localhost:%d/" (-> system :http-listener-listener :port))]
+                 (println (format "Browsing at %s" url))
+                 (clojure.java.browse/browse-url url)
+                 )))))
