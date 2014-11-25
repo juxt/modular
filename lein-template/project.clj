1f088f9b001a6dd6b2c14381ae63acea2684c550
Malcolm Sparks <malcolm@juxt.pro>
2014-11-25 23:10:12 +0000

    Releasing lein-template

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,7 +1,7 @@
 ;; Copyright © 2014 JUXT LTD.
 
 ;; We call this project modular to make the invocation: lein new modular appname
-(defproject modular/lein-template "0.6.5"
+(defproject modular/lein-template "0.6.6"
   :description "Leiningen template for a full-featured component based app using modular extensions."
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
