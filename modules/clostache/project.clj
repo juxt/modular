428b596707ae200cdddbb2cb127925334ea02852
Malcolm Sparks <malcolm@juxt.pro>
2014-11-25 09:49:58 +0000

    Clostache release 0.6.0

--- modules/clostache/project.clj
+++ modules/clostache/project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/clostache "0.6.0-SNAPSHOT"
+(defproject juxt.modular/clostache "0.6.0"
   :description "A modular extension that provides a Clostache templater for juxt.modular/template"
   :url "https://github.com/juxt/modular/tree/master/modules/clostache"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/component "0.2.1"]
                  [de.ubercode.clostache/clostache "1.3.1"]
-                 [juxt.modular/template "0.6.0-SNAPSHOT"]
+                 [juxt.modular/template "0.6.0"]
                  ])
