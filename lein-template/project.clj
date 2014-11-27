bdc6aa7388aa72c56940e3b77778a94ba5cdb666
Malcolm Sparks <malcolm@juxt.pro>
2014-11-27 17:24:31 +0000

    Upgraded to Clojure 1.7.0alpha4

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -5,7 +5,7 @@
   :description "Leiningen template for a full-featured component based app using modular extensions."
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  ;; EDN reader with location metadata
                  [org.clojure/tools.reader "0.8.3"]
                  [org.clojure/tools.logging "0.2.6"]]
