0218daa0f25344304890b266e7fe6248ef4bb1a2
Malcolm Sparks <malcolm@juxt.pro>
2014-11-27 16:05:14 +0000

    Add logging dependency to modular.ring

--- modules/ring/project.clj
+++ modules/ring/project.clj
@@ -1,8 +1,9 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/ring "0.5.1"
+(defproject juxt.modular/ring "0.5.2-SNAPSHOT"
   :description "A modular extension that represents the Ring interface"
   :url "https://github.com/juxt/modular/tree/master/modules/ring"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[prismatic/schema "0.2.1"]])
+  :dependencies [[prismatic/schema "0.2.1"]
+                 [org.clojure/tools.logging "0.2.6"]])
