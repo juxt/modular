48144de3a4a6eb4b0f65a25a869535d3f44e8e16
Malcolm Sparks <malcolm@juxt.pro>
2014-11-27 23:54:44 +0000

    async release

--- modules/async/project.clj
+++ modules/async/project.clj
@@ -1,13 +1,10 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/async "0.5.2"
+(defproject juxt.modular/async "0.5.3"
   :description "A modular extension that provides support core.async"
   :url "https://github.com/juxt/modular/tree/master/modules/http-kit"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [
-                 [prismatic/schema "0.2.1"]
-                 #_[org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 #_[org.clojure/core.async "0.1.303.0-886421-alpha"]
+  :dependencies [[prismatic/schema "0.2.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 ])
+                 [org.clojure/tools.logging "0.2.6"]])
