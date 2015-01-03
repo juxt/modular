6304581d88c0b489309348a68f482322d524179b
Malcolm Sparks <malcolm@juxt.pro>
2015-01-03 00:38:38 +0000

    Ready to release modular.aleph

--- modules/aleph/project.clj
+++ modules/aleph/project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/aleph "0.0.1-SNAPSHOT"
+(defproject juxt.modular/aleph "0.0.1"
   :description "A modular extension that provides support for aleph"
   :url "https://github.com/juxt/modular/tree/master/modules/aleph"
   :license {:name "The MIT License"
