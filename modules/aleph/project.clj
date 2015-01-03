2b3150aa872c79201edf33fe4b1f5806fbd08045
Malcolm Sparks <malcolm@juxt.pro>
2015-01-03 00:44:37 +0000

    Rerelease aleph

--- modules/aleph/project.clj
+++ modules/aleph/project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/aleph "0.0.1"
+(defproject juxt.modular/aleph "0.0.2"
   :description "A modular extension that provides support for aleph"
   :url "https://github.com/juxt/modular/tree/master/modules/aleph"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[aleph "0.4.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
+  :dependencies [[aleph "0.4.0-alpha9" :exclusions [org.clojure/clojure]]
                  [prismatic/schema "0.2.1"]
                  [juxt.modular/ring "0.5.2"]
                  ]
