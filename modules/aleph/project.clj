31ab22000ac56297aef4837a587d257c575b5951
Malcolm Sparks <malcolm@juxt.pro>
2015-01-30 16:59:40 +0000

    Upgrading to latest aleph

--- modules/aleph/project.clj
+++ modules/aleph/project.clj
@@ -1,12 +1,12 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/aleph "0.0.2"
+(defproject juxt.modular/aleph "0.0.3"
   :description "A modular extension that provides support for aleph"
   :url "https://github.com/juxt/modular/tree/master/modules/aleph"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[aleph "0.4.0-alpha9" :exclusions [org.clojure/clojure]]
-                 [prismatic/schema "0.2.1"]
+  :dependencies [[aleph "0.4.0-beta1" :exclusions [org.clojure/clojure]]
+                 [prismatic/schema "0.3.5"]
                  [juxt.modular/ring "0.5.2"]
                  ]
   :profiles {:dev {:dependencies
