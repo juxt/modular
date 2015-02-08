9ad352a5c309399fbdeb759569faf4e72e60f797
Malcolm Sparks <malcolm@juxt.pro>
2015-02-08 21:21:14 +0000

    Releasing 0.8.0

--- modules/bidi/project.clj
+++ modules/bidi/project.clj
@@ -1,12 +1,12 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/bidi "0.8.0-SNAPSHOT"
+(defproject juxt.modular/bidi "0.8.0"
   :description "A modular extension that provides support for bidi routing"
   :url "https://github.com/juxt/modular/tree/master/modules/bidi"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/component "0.2.1"]
-                 [bidi "1.18.0-SNAPSHOT"]
+                 [bidi "1.18.0"]
                  [juxt.modular/ring "0.5.2"]
                  [prismatic/schema "0.3.3"]
                  [prismatic/plumbing "0.3.5"]]
