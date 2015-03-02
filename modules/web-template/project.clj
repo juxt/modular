bb6b42faeeec533c37feb6cc40760eb962696a29
Malcolm Sparks <malcolm@juxt.pro>
2015-03-02 15:29:27 +0000

    Updating web-template

--- modules/web-template/project.clj
+++ modules/web-template/project.clj
@@ -1,10 +1,10 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/web-template "0.5.2"
+(defproject juxt.modular/web-template "0.6.2"
   :description "A modular extension that supports template data that is determined by the web request"
   :url "https://github.com/juxt/modular/tree/master/modules/template"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[com.stuartsierra/component "0.2.1"]
-                 [juxt.modular/template "0.5.0"]
+  :dependencies [[com.stuartsierra/component "0.2.2"]
+                 [juxt.modular/template "0.6.2"]
                  ])
