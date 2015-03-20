16e8ec68ab82c86e565f5f5cfb2cb0d2b36237e3
Malcolm Sparks <malcolm@juxt.pro>
2015-03-20 14:26:15 +0000

    Update to latest http-kit

--- modules/http-kit/project.clj
+++ modules/http-kit/project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/http-kit "0.5.3"
+(defproject juxt.modular/http-kit "0.5.4"
   :description "A modular extension that provides support for http-kit channels"
   :url "https://github.com/juxt/modular/tree/master/modules/http-kit"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[http-kit "2.1.16"]
-                 [prismatic/schema "0.2.1"]
+  :dependencies [[http-kit "2.1.18"]
+                 [prismatic/schema "0.2.2"]
                  [juxt.modular/ring "0.5.2"]
                  ])
