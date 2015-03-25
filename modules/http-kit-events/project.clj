2d19f8825db982f681e92064e23b085de1441dc5
Malcolm Sparks <malcolm@juxt.pro>
2015-03-25 16:29:10 +0000

    Upgrade libs

--- modules/http-kit-events/project.clj
+++ modules/http-kit-events/project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/http-kit-events "0.6.0"
+(defproject juxt.modular/http-kit-events "0.6.1"
   :description "A modular extension that provides support for events over http-kit channels"
   :url "https://github.com/juxt/modular/tree/master/modules/http-kit-events"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[juxt.modular/http-kit "0.5.3"]
-                 [juxt.modular/bidi "0.9.0"]
+  :dependencies [[juxt.modular/http-kit "0.5.4"]
+                 [juxt.modular/bidi "0.9.2"]
                  [juxt.modular/async "0.5.3"]
                  ])
