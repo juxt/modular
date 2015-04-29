63bef15a13a75ff7be6930fc5fe956a89cb8768d
Sébastien Fievet <_@sebastien-fievet.fr>
2015-04-29 09:42:07 +0200

    Fix datomic project URL

--- modules/datomic/project.clj
+++ modules/datomic/project.clj
@@ -2,7 +2,7 @@
 
 (defproject juxt.modular/datomic "0.2.1"
   :description "A modular extension that provides support for Datomic"
-  :url "https://github.com/juxt/modular/tree/master/modules/bidi"
+  :url "https://github.com/juxt/modular/tree/master/modules/datomic"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[prismatic/schema "0.2.1"]
