8ad45954fd64ec8c10a0d4d523f11ac6d26fe4e7
Sébastien Fievet <_@sebastien-fievet.fr>
2015-04-29 09:43:31 +0200

    Fix menu project URL

--- modules/menu/project.clj
+++ modules/menu/project.clj
@@ -2,7 +2,7 @@
 
 (defproject juxt.modular/menu "0.5.3"
   :description "A modular extension that provides support for menus, navbars, and other navigation elements that house hyperlinks."
-  :url "https://github.com/juxt/modular/tree/master/modules/nav"
+  :url "https://github.com/juxt/modular/tree/master/modules/menu"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/component "0.2.1"]
