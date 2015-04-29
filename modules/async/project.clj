5a223357426b5f88aceb422297e5c9a3c2c696b7
Sébastien Fievet <_@sebastien-fievet.fr>
2015-04-29 09:41:04 +0200

    Fix async project URL

--- modules/async/project.clj
+++ modules/async/project.clj
@@ -2,7 +2,7 @@
 
 (defproject juxt.modular/async "0.5.3"
   :description "A modular extension that provides support core.async"
-  :url "https://github.com/juxt/modular/tree/master/modules/http-kit"
+  :url "https://github.com/juxt/modular/tree/master/modules/async"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[prismatic/schema "0.2.1"]
