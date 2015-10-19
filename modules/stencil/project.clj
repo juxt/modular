11dea1776288325ef3b9bd7d0c8b20da7b929d0d
Malcolm Sparks <malcolm@juxt.pro>
2015-10-19 12:07:37 +0100

    Upgrade stencil

--- modules/stencil/project.clj
+++ modules/stencil/project.clj
@@ -1,11 +1,11 @@
 ;; Copyright © 2015 JUXT LTD.
 
-(defproject juxt.modular/stencil "0.1.0"
+(defproject juxt.modular/stencil "0.1.1"
   :description "A modular extension that provides a Stencil templater for juxt.modular/template"
   :url "https://github.com/juxt/modular/tree/master/modules/stencil"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[com.stuartsierra/component "0.2.2"]
-                 [stencil "0.4.0" :exclusions [slingshot]]
+                 [stencil "0.5.0"]
                  [juxt.modular/template "0.6.3"]
                  ])
