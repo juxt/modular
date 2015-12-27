a3a68f0a13755b32c8f69e127c3f9c2d988a3faa
Malcolm Sparks <malcolm@juxt.pro>
2015-12-27 14:35:55 +0000

    Update aleph module dependencies

--- modules/aleph/project.clj
+++ modules/aleph/project.clj
@@ -1,13 +1,13 @@
 ;; Copyright © 2014 JUXT LTD.
 
-(defproject juxt.modular/aleph "0.1.3"
+(defproject juxt.modular/aleph "0.1.4"
   :description "A modular extension that provides support for aleph"
   :url "https://github.com/juxt/modular/tree/master/modules/aleph"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[aleph "0.4.0" :exclusions [org.clojure/clojure]]
-                 [prismatic/schema "0.4.3"]
-                 [juxt.modular/ring "0.5.2" :exclusions [prismatic/schema ]]]
+  :dependencies [[aleph "0.4.1-beta3" :exclusions [org.clojure/clojure]]
+                 [prismatic/schema "1.0.4"]
+                 [juxt.modular/ring "0.5.3" :exclusions [prismatic/schema]]]
   :profiles {:dev {:dependencies
-                   [[org.clojure/clojure "1.7.0-RC1"]
-                    [com.stuartsierra/component "0.2.3"]]}})
+                   [[org.clojure/clojure "1.7.0"]
+                    [com.stuartsierra/component "0.3.1"]]}})
